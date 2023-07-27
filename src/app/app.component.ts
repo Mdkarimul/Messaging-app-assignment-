fa531be0a85c325c16507f1ff00a298ba61d4e4d
karimul islam <7mdkarimul@gmail.com>
2023-07-27 15:06:58 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './services/http.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'assignment';
+  public profileData:any;
+  public postData:any;
+  public pageNo:number =1;
+  constructor (private httpService: HttpService){
+  
+  }
+
+  ngOnInit(): void {
+    alert("init");
+   this.getData();
+   this.getPost(this.pageNo);
+  }
+
+  getData():void{
+    this.httpService.getProfile().subscribe((data:any)=>{
+    this.profileData = data;
+    console.log(this.profileData);
+    });
+  }
+
+  getPost(pageNo:number):void{
+    this.httpService.getPost(pageNo).subscribe((data:any)=>{
+    this.postData = data;
+    console.log(this.postData);
+    }); 
+}
+
+next():void{
+this.pageNo >= 4 ? this.pageNo=1 : this.pageNo = this.pageNo+1;
+this.getPost(this.pageNo);
+
+}
+prev():void{
+if(this.pageNo >1){
+  this.pageNo--;
+  alert(this.pageNo);
+  this.getPost(this.pageNo);
+}
+}
+
+actualPage(page:number){
+   this.pageNo = page;
+   this.getPost(this.pageNo);
+}
+
+
+
 }
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders,HttpParams } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+ private headers?:HttpHeaders;
+  constructor(private http:HttpClient) { }
+  
+ getProfile(){
+    const url = "https://app.smartkeeda.com/demoapi/demo/main";
+    const queryParam = new HttpParams();
+    const Param = queryParam.append("PageNo",2)
+                            .append("Token","123")
+                            .append("AppVersion",100)
+                            .append("GroupId",1);
+
+    return  this.http.post<any>(url,"",{params:Param});
+ }
+
+ getPost(pageNo:Number){
+  console.log(pageNo);
+  const queryParam = new HttpParams();
+  const Param = queryParam.append("PageNo",pageNo.toString())
+                          .append("Token","123")
+                          .append("AppVersion",100)
+                          .append("GroupId",1);
+
+  const url = "https://app.smartkeeda.com/demoapi/demo/PostData";
+  return  this.http.post<any>(url,"",{params:Param});
+ }
+
+
+}
